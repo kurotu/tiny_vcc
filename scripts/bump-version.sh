e8d8e06212de355211512c94255ddf9bb3b4b560
kurotu <kurotu@gmail.com>
2022-12-22 20:37:04 +0900

    Update changelogs in bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,5 +3,7 @@ set -eu
 
 VERSION="${1}"
 sed -i -b -e "s/^version:.*/version: ${VERSION}/g" pubspec.yaml
+sed -i -b -e "s/\[Unreleased\]/\[${VERSION}\] - $(date -I)/g" CHANGELOG*.md
+
 git commit -am "Version ${VERSION}"
 git tag "v${VERSION}"
