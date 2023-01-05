6ceae56d7986194fcf22f84c8823f07388a9f12d
kurotu <kurotu@gmail.com>
2023-01-06 02:26:27 +0900

    Automate downloading .NET SDK installer

--- lib/routes/requirements_route.dart
+++ lib/routes/requirements_route.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:system_info2/system_info2.dart';
+import 'package:url_launcher/link.dart';
 
 import '../globals.dart';
 import '../providers.dart';
 import '../utils.dart';
+import '../utils/system_info.dart';
 
 enum RequirementState { ok, ng, notChecked }
 
@@ -67,7 +74,10 @@ final _unityStateProvider = FutureProvider.autoDispose((ref) async {
 class RequirementsRoute extends ConsumerWidget {
   static const routeName = '/requirements';
 
-  const RequirementsRoute({super.key});
+  RequirementsRoute({super.key});
+
+  final _dotnetDownloadPageUri =
+      Uri.parse('https://dotnet.microsoft.com/download/dotnet/6.0');
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -76,6 +86,26 @@ class RequirementsRoute extends ConsumerWidget {
     final vpmState = ref.watch(_vpmStateProvider);
     final hubState = ref.watch(_unityHubStateProvider);
     final unityState = ref.watch(_unityStateProvider);
+    ref.listen(_dotNetStateProvider, (previous, next) {
+      if (!next.isLoading && next.valueOrNull == StepState.error) {
+        ref.read(_stepProvider.notifier).state = _StepIndex.dotnet;
+      }
+    });
+    ref.listen(_vpmStateProvider, (previous, next) {
+      if (!next.isLoading && next.valueOrNull == StepState.error) {
+        ref.read(_stepProvider.notifier).state = _StepIndex.vpm;
+      }
+    });
+    ref.listen(_unityHubStateProvider, (previous, next) {
+      if (!next.isLoading && next.valueOrNull == StepState.error) {
+        ref.read(_stepProvider.notifier).state = _StepIndex.unityHub;
+      }
+    });
+    ref.listen(_unityStateProvider, (previous, next) {
+      if (!next.isLoading && next.valueOrNull == StepState.error) {
+        ref.read(_stepProvider.notifier).state = _StepIndex.unity;
+      }
+    });
 
     return Scaffold(
       appBar: AppBar(title: const Text('Requirements')),
@@ -90,7 +120,17 @@ class RequirementsRoute extends ConsumerWidget {
             title: const Text('.NET 6.0 SDK'),
             content: Container(
                 alignment: Alignment.centerLeft,
-                child: Text('Install .NET 6.0 SDK.')),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                          'Install .NET 6.0 SDK. You can also download the SDK installer from web.'),
+                      Link(
+                          uri: _dotnetDownloadPageUri,
+                          builder: (context, followLink) => TextButton(
+                              onPressed: followLink,
+                              child: Text(_dotnetDownloadPageUri.toString())))
+                    ])),
             state: _stepState(dotnetState),
           ),
           Step(
@@ -167,11 +207,12 @@ class RequirementsRoute extends ConsumerWidget {
       BuildContext context, WidgetRef ref, _StepIndex step) async {
     switch (step) {
       case _StepIndex.dotnet:
-        print('TODO: Handle this case.');
-        final dialog = showProgressDialog(
-            context, Theme.of(context), 'Installing .NET 6.0 SDK.');
-        await Future.delayed(const Duration(seconds: 5));
-        dialog.close();
+        try {
+          await _installDotNetSdk(context, ref);
+        } on Exception catch (error) {
+          await showSimpleErrorDialog(
+              context, 'Failed to install .NET SDK', error);
+        }
         _refresh(ref);
         break;
       case _StepIndex.vpm:
@@ -191,6 +232,70 @@ class RequirementsRoute extends ConsumerWidget {
       case _StepIndex.complete:
         print('TODO: Handle this case.');
         break;
+    }
+  }
+
+  static Future<bool> _installDotNetSdk(
+      BuildContext context, WidgetRef ref) async {
+    final dialog = showProgressDialog(
+        context, Theme.of(context), 'Downloading .NET 6.0 SDK installer.');
+    File? installer;
+    try {
+      if (SystemInfo.arch == Architecture.unknown) {
+        throw Exception(
+            "Failed to detect architecture for ${SysInfo.cores[0].architecture}.");
+      }
+
+      final tmp = await getTemporaryDirectory();
+      final dir = Directory(p.join(tmp.path, 'tiny_vcc'));
+      await dir.create(recursive: true);
+
+      final dotnet = ref.read(dotNetServiceProvider);
+      final version = await dotnet.getLatestVersion();
+      Uri installerUri;
+      if (Platform.isWindows) {
+        installer = File(p.join(dir.path,
+            'dotnet-sdk-installer-${DateTime.now().millisecondsSinceEpoch}.exe'));
+        installerUri = dotnet.getWindowsInstallerUri(version, SystemInfo.arch);
+      } else if (Platform.isMacOS) {
+        installer = File(p.join(dir.path,
+            'dotnet-sdk-installer-${DateTime.now().millisecondsSinceEpoch}.pkg'));
+        installerUri = dotnet.getMacInstallerUri(version, SystemInfo.arch);
+      } else if (Platform.isLinux) {
+        throw UnimplementedError('Need to implement dotnet installer.');
+      } else {
+        throw Error();
+      }
+
+      logger?.i(
+          'Downloading dotnet sdk installer from $installerUri to $installer.');
+      final client = http.Client();
+      final request = http.Request('GET', installerUri);
+      final res = await client.send(request);
+      if (res.statusCode / 100 != 2) {
+        throw HttpException('Failed to get', uri: installerUri);
+      }
+      final sink = installer.openWrite();
+      await sink.addStream(res.stream);
+      await sink.close();
+      logger?.i(
+          'Downloaded dotnet sdk installer from $installerUri to $installer.');
+
+      dialog.update(value: 1, msg: 'Installing .NET 6.0 SDK.');
+      logger?.i('Executing installer: $installer');
+      final result = await Process.run(installer.path, []);
+      logger?.i('Finished installer with code ${result.exitCode}: $installer');
+
+      return result.exitCode == 0;
+    } on Exception catch (error) {
+      logger?.e(error.toString());
+      rethrow;
+    } finally {
+      if (await installer?.exists() == true) {
+        await installer?.delete();
+      }
+      dialog.close();
+      await Future.delayed(const Duration());
     }
   }
 }
