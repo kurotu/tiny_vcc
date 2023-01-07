a79c69841ba2bcb75ead817ce501ba4a1708565a
kurotu <kurotu@gmail.com>
2023-01-08 01:19:35 +0900

    Unify requirements state providers

--- lib/widgets/projects_page.dart
+++ lib/widgets/projects_page.dart
@@ -13,34 +13,6 @@ import '../services/vcc_service.dart';
 import '../utils.dart';
 import '../utils/platform_feature.dart';
 
-final _readyToUseProvider = FutureProvider.autoDispose((ref) async {
-  // Quick check for startup.
-  final vcc = ref.read(vccServiceProvider);
-  if (!vcc.isInstalled()) {
-    return false;
-  }
-
-  if (await vcc.getCliVersion() < requiredVpmVersion) {
-    return false;
-  }
-
-  final settings = ref.watch(vccSettingsProvider);
-  if (settings.hasError) {
-    return false;
-  }
-  if (settings.isLoading) {
-    return true;
-  }
-
-  if (!await (File(settings.requireValue.pathToUnityHub).exists())) {
-    return false;
-  }
-  if (!await (File(settings.requireValue.pathToUnityExe).exists())) {
-    return false;
-  }
-  return true;
-});
-
 class ProjectsPage extends ConsumerWidget {
   const ProjectsPage({super.key});
 
@@ -146,9 +118,13 @@ class ProjectsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen(_readyToUseProvider, (previous, next) {
+    ref.listen(readyToUseProvider, (previous, next) {
       if (next.valueOrNull == false) {
         Navigator.of(context).pushReplacementNamed(RequirementsRoute.routeName);
+        ref.refresh(dotNetStateProvider);
+        ref.refresh(vpmStateProvider);
+        ref.refresh(unityHubStateProvider);
+        ref.refresh(unityStateProvider);
       }
     });
     final settings = ref.watch(vccSettingsProvider);
