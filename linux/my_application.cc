7e73b9fadfec803f9c863b1108b98466b1d3edf3
kurotu <kurotu@gmail.com>
2023-01-18 02:26:49 +0900

    Set application id and window title for Linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "tiny_vcc");
+    gtk_header_bar_set_title(header_bar, "Tiny VCC");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "tiny_vcc");
+    gtk_window_set_title(window, "Tiny VCC");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
